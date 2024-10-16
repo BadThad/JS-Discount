3476271cf37e29d9fa2ad37bfcb31a7f99857804
Peter Fänge <pellefange@gmail.com>
2024-10-16 16:59:04 +0200

    added method to show the final price to two decimal places

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ let itemprice = prompt ("Please enter the price of your item (remember to includ
 
 let discount = parseFloat(itemprice.slice(1)*0.9)
 
-console.log("Congratulations! You are eligible for our 10% discount! The price on your item is now $" + discount);
+// 10% of $49.99 is $44.991 so I added the following line to round it to two decimals.
+
+let newprice = discount.toFixed(2)
+
+console.log("Congratulations! You are eligible for our 10% discount! The price on your item is now $" + newprice);
